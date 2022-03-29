5a6b81bd7524e6407469c67f3b6184c3fe24ccfa
Oscar <oscar7822002@gmail.com>
2022-03-29 17:20:40 -0500

    Consulta a la base de datos con Prisma y getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,24 @@
+import { PrismaClient } from '@prisma/client'
 import Head from 'next/head'
 import Image from 'next/image'
 
-export default function Home() {
+export default function Home({categorias}) {
+  
+  console.log(categorias)
+  
   return (
     <h1>Next.Js</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+
+  const categorias = await prisma.categoria.findMany()
+
+  return {
+    props: {
+      categorias,
+    }
+  }
 }
