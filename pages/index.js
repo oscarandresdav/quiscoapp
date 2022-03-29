5034057afc1f541730347b98d066150f66e80862
Oscar <oscar7822002@gmail.com>
2022-03-29 17:47:35 -0500

    Creando Layout Principal

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,12 @@
-import { PrismaClient } from '@prisma/client'
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../layout/Layout'
 
-export default function Home({categorias}) {
-  
-  console.log(categorias)
+export default function Home() {
   
   return (
-    <h1>Next.Js</h1>
+    <Layout>
+      <h1>Inicio</h1>
+    </Layout>
   )
-}
-
-export const getServerSideProps = async () => {
-  const prisma = new PrismaClient()
-
-  const categorias = await prisma.categoria.findMany()
-
-  return {
-    props: {
-      categorias,
-    }
-  }
 }
