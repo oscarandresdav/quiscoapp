f04f3f70194d61f2a57a0fd588e70fa1ab1860b4
Oscar <oscar7822002@gmail.com>
2022-03-30 18:14:40 -0500

    Detectando Click a producto

--- components/Producto.js
+++ components/Producto.js
@@ -1,9 +1,12 @@
 import Image from "next/image"
 import { formatearDinero } from "../helpers"
+import useQuiosco from "../hooks/useQuiosco"
 
 const Producto = ({producto}) => {
 
   const { nombre, precio, imagen } = producto
+
+  const { handleSetProducto, handleChangeModal } = useQuiosco()
 
   return (
     <div className="border p-3">
@@ -14,6 +17,17 @@ const Producto = ({producto}) => {
         <p className="mt-5 font-black text-4xl text-amber-500">
           {formatearDinero(precio)}
         </p>
+
+        <button
+          type="button"
+          className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold"
+          onClick={() => {
+            handleChangeModal()
+            handleSetProducto(producto)
+          }}
+        >
+          Agregar
+        </button>
       </div>
     </div>
   )
--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -7,31 +7,45 @@ const QuioscoProvider = ({children}) => {
 
   const [categorias, setCategorias] = useState([])
   const [categoriaActual, setCategoriaActual] = useState({})
+  const [producto, setProducto] = useState({})
+  const [modal, setModal] = useState(false)
 
   const obtenerCategorias = async () => {
     const { data } = await axios('/api/categorias')
     setCategorias(data)
   }
 
+  useEffect(() => {
+    obtenerCategorias()
+  }, [])
+  
+  useEffect(() => {
+    setCategoriaActual(categorias[0])
+  }, [categorias])
+  
   const handleClickCategoria = id => {
     const categoria = categorias.filter(cat => cat.id === id)
     setCategoriaActual(categoria[0]);
   }
 
-  useEffect(() => {
-    obtenerCategorias()
-  }, [])
+  const handleSetProducto = producto => {
+    setProducto(producto)
+  }
 
-  useEffect(() => {
-    setCategoriaActual(categorias[0])
-  }, [categorias])
+  const handleChangeModal = () => {
+    setModal(!modal)
+  }
 
   return (
     <QuioscoContext.Provider
       value={{
         categorias,
         categoriaActual,
-        handleClickCategoria
+        handleClickCategoria,
+        producto,
+        handleSetProducto,
+        modal,
+        handleChangeModal
       }}
     >
       {children}
